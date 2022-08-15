e5111dfe1a92dadba4231b1206dddfe4fe958934
AdamsFlorisbal <adamsgawlinski@hotmail.com>
2022-08-15 19:42:15 -0300

    Aviso

---
+++ src/components/firebase/header/Aviso.js
@@ -0,0 +1,36 @@
+import aviso from './aviso.css'
+import React from 'react';
+
+export const Aviso = () => {
+    return (
+    <>
+    <div ClassName="container__aviso">
+    <ul>
+        <li>E</li>
+        <li>M</li>
+        <li>★</li>
+        <li>D</li>
+        <li>E</li>
+        <li>S</li>
+        <li>E</li>
+        <li>N</li>
+        <li>V</li>
+        <li>O</li>
+        <li>L</li>
+        <li>V</li>
+        <li>I</li>
+        <li>M</li>
+        <li>E</li>
+        <li>N</li>
+        <li>T</li>
+        <li>O</li>
+
+    </ul>
+    </div>
+    </>
+    )
+}
+
+
+
+
